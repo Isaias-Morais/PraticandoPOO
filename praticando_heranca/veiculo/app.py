0e3fca39f42e64f38e1ed26f7ad965538fbbd547
Isaias morais <isaiassenac10@gmail.com>
2025-12-01 06:52:30 -0300

    heranças Veiculos

--- praticando_heranca/veiculo/app.py
+++ praticando_heranca/veiculo/app.py
@@ -1,7 +1,7 @@
 from praticando_heranca.veiculo.moto import Moto
 from praticando_heranca.veiculo.carro import Carro
 
-carro1 = Carro('Honda',"Civic",4,'Preto')
+carro1 = Carro('Honda',"Civic",4,'Prata')
 moto1 = Moto("Honda",'Africa Twin','BigTrail',"Preto")
 
 print(carro1)
